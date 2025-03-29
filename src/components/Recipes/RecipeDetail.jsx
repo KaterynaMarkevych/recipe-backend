3f75f989ae324aa107c06c06dabe9fe0b923774f
KaterynaMarkevych <katya.markevich73@gmail.com>
2025-03-29 12:10:48 +0200

    modify styles

--- src/components/Recipes/RecipeDetail.jsx
+++ src/components/Recipes/RecipeDetail.jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./RecipeDetail.module.scss";
 
 // Define colors for difficulty badges outside the component
 const difficultyColors = {
@@ -130,10 +131,10 @@ const RecipeDetail = ({ id, initialData = {} }) => {
   }, [recipe]);
 
   return (
-    <div className="container mx-auto px-4 py-12">
+    <div className="container mx-auto px-4 pb-12">
       <Link
         href="/recipes"
-        className="inline-flex items-center text-blue-600 hover:text-blue-800 transition-colors mb-6"
+        className="inline-flex items-center text-blue-400 hover:text-blue-600 transition-colors mb-6"
         tabIndex="0"
         aria-label="Повернутися до всіх рецептів"
       >
@@ -152,17 +153,17 @@ const RecipeDetail = ({ id, initialData = {} }) => {
         Повернутися до всіх рецептів
       </Link>
 
-      <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+      <div className={styles.container}>
         <div className="md:flex">
-          <div className="md:w-1/2">
+          <div className={styles.imagesContainer}>
             {recipe.image ? (
-              <div className="relative w-full h-[300px] md:h-[500px]">
+              <div className="relative w-full">
                 <Image
                   src={recipe.image}
                   alt={recipe.title}
-                  fill
-                  sizes="(max-width: 768px) 100vw, 50vw"
-                  className="object-cover"
+                  width={500}
+                  height={300}
+                  className={styles.image}
                   priority
                 />
               </div>
@@ -171,56 +172,68 @@ const RecipeDetail = ({ id, initialData = {} }) => {
                 <span className="text-gray-400 text-lg">Немає зображення</span>
               </div>
             )}
+
+            <div className={styles.userWrapper}>
+              {authorLoading && <p>Завантаження користувача...</p>}
+              {authorError && <p>{authorError}</p>}
+              {author && (
+                <div className={styles.userContainer}>
+                  <img
+                    src={author.avatar}
+                    alt={author.username}
+                    className={styles.userAvatar}
+                  />
+                  <p className={styles.userName}>{author.username}</p>
+                </div>
+              )}
+            </div>
           </div>
 
-          <div>
-            {authorLoading && <p>Завантаження користувача...</p>}
-            {authorError && <p>{authorError}</p>}
-            {author && (
-              <div>
-                <img src={author.avatar} alt={author.username} />
-                <p>{author.username}</p>
-              </div>
-            )}
-          </div>
-
-          <div className="md:w-1/2 p-6">
-            <h1 className="text-3xl font-bold mb-4">{recipe.title}</h1>
-            <div className="flex flex-wrap gap-2 mb-4">
-              {recipe.type && (
-                <span className="bg-black/70 text-white px-2 py-1 text-xs rounded-full">
-                  {recipe.type}
-                </span>
-              )}
-
-              {recipe.difficulty && (
-                <span
-                  className={`px-2 py-1 rounded-full text-xs ${
-                    difficultyColors[recipe.difficulty]
-                  }`}
-                >
-                  {recipe.difficulty}
-                </span>
-              )}
-
-              {recipe.diet &&
-                recipe.diet.map((dietType, index) => (
-                  <span
-                    key={index}
-                    className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded-full"
-                  >
-                    {dietType}
+          <div className={styles.infoWrapper}>
+            <h2 className="text-3xl font-bold mb-4">{recipe.title}</h2>
+            <div className={styles.infoContainer}>
+              <p className={styles.infoItem}>
+                Тип страви:
+                {recipe.type && (
+                  <span className="bg-black/70 text-white px-2 py-1 text-xs rounded-full ml-1 sm:text-sm md:text-base lg:text-lg">
+                    {recipe.type}
                   </span>
-                ))}
+                )}
+              </p>
+              <p className={styles.infoItem}>
+                Складність приготування:
+                {recipe.difficulty && (
+                  <span
+                    className={`px-2 py-1 rounded-full text-xs ml-1 sm:text-sm md:text-base lg:text-lg ${
+                      difficultyColors[recipe.difficulty]
+                    }`}
+                  >
+                    {recipe.difficulty}
+                  </span>
+                )}
+              </p>
+
+              <p className={styles.infoItem}>
+                Дієти:
+                {recipe.diet &&
+                  recipe.diet.map((dietType, index) => (
+                    <span
+                      key={index}
+                      className="bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded-full  ml-1 sm:text-sm md:text-base lg:text-lg"
+                    >
+                      {dietType}
+                    </span>
+                  ))}
+              </p>
             </div>
 
-            <p className="text-gray-600 mb-6">{recipe.description}</p>
+            <p className={styles.description}>{recipe.description}</p>
 
             <div className="grid grid-cols-2 gap-4 mb-6">
               <div className="flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-500 mr-2"
+                  className="h-5 w-5 md:h-7 md:w-7 lg:h-9 lg:w-9 text-gray-800 mr-2"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -233,15 +246,17 @@ const RecipeDetail = ({ id, initialData = {} }) => {
                   />
                 </svg>
                 <div>
-                  <div className="text-sm text-gray-500">Час приготування</div>
-                  <div className="font-medium">{recipe.cookingTime} хв</div>
+                  <p className={styles.titles}>Час приготування</p>
+                  <p className="font-medium text-base md:text-lg lg:text-xl">
+                    {recipe.cookingTime} хв
+                  </p>
                 </div>
               </div>
 
               <div className="flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-500 mr-2"
+                  className="h-5 w-5 md:h-7 md:w-7 lg:h-9 lg:w-9 text-gray-700 mr-2"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -254,15 +269,17 @@ const RecipeDetail = ({ id, initialData = {} }) => {
                   />
                 </svg>
                 <div>
-                  <div className="text-sm text-gray-500">Порції</div>
-                  <div className="font-medium">{recipe.servings} порц.</div>
+                  <p className={styles.titles}>Порції</p>
+                  <p className="font-medium text-base md:text-lg lg:text-xl">
+                    {recipe.servings} порц.
+                  </p>
                 </div>
               </div>
 
               <div className="flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-gray-500 mr-2"
+                  className="h-5 w-5 md:h-7 md:w-7 lg:h-9 lg:w-9 text-gray-700 mr-2"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
@@ -275,63 +292,71 @@ const RecipeDetail = ({ id, initialData = {} }) => {
                   />
                 </svg>
                 <div>
-                  <div className="text-sm text-gray-500">Кухня</div>
-                  <div className="font-medium">
+                  <p className={styles.titles}>Кухня</p>
+                  <p className="font-medium text-base md:text-lg lg:text-xl">
                     {recipe.cuisine || "Не вказано"}
-                  </div>
+                  </p>
                 </div>
               </div>
 
               <div className="flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5 text-yellow-400 mr-2"
+                  className="h-5 w-5 md:h-7 md:w-7 lg:h-9 lg:w-9 text-yellow-400 mr-2"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                 >
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
                 <div>
-                  <div className="text-sm text-gray-500">Рейтинг</div>
-                  <div className="font-medium">
+                  <p className={styles.titles}>Рейтинг</p>
+                  <p className="font-medium text-base md:text-lg lg:text-xl">
                     {recipe.rating?.toFixed(1) || "0.0"}
-                  </div>
+                  </p>
                 </div>
               </div>
             </div>
+          </div>
+        </div>
 
-            <div className="border-t border-gray-200 pt-6">
-              <h3 className="text-lg font-medium mb-2">
-                Харчова цінність (на порцію)
-              </h3>
-              <div className="grid grid-cols-4 gap-2">
-                <div className="bg-gray-50 p-2 rounded-md text-center">
-                  <div className="text-sm text-gray-500">Калорії</div>
-                  <div className="font-medium">
-                    {recipe.nutrition?.calories || "0"} ккал
-                  </div>
-                </div>
+        <div className="border-t border-gray-600 pt-6">
+          <h3 className="text-lg font-medium mb-2 md:text-xl lg:text-2xl">
+            Харчова цінність (на порцію)
+          </h3>
+          <div className="grid grid-cols-4 gap-2">
+            <div className="bg-gray-50 p-2 rounded-md text-center">
+              <div className="text-sm text-gray-500 md:text-base lg:text-lg">
+                Калорії
+              </div>
+              <div className="font-medium md:text-lg lg:text-xl">
+                {recipe.nutrition?.calories || "0"} ккал
+              </div>
+            </div>
 
-                <div className="bg-gray-50 p-2 rounded-md text-center">
-                  <div className="text-sm text-gray-500">Білки</div>
-                  <div className="font-medium">
-                    {recipe.nutrition?.protein || "0"} г
-                  </div>
-                </div>
+            <div className="bg-gray-50 p-2 rounded-md text-center">
+              <div className="text-sm text-gray-500 md:text-base lg:text-lg">
+                Білки
+              </div>
+              <div className="font-medium md:text-lg lg:text-xl">
+                {recipe.nutrition?.protein || "0"} г
+              </div>
+            </div>
 
-                <div className="bg-gray-50 p-2 rounded-md text-center">
-                  <div className="text-sm text-gray-500">Жири</div>
-                  <div className="font-medium">
-                    {recipe.nutrition?.fats || "0"} г
-                  </div>
-                </div>
+            <div className="bg-gray-50 p-2 rounded-md text-center">
+              <div className="text-sm text-gray-500 md:text-base lg:text-lg">
+                Жири
+              </div>
+              <div className="font-medium md:text-lg lg:text-xl">
+                {recipe.nutrition?.fats || "0"} г
+              </div>
+            </div>
 
-                <div className="bg-gray-50 p-2 rounded-md text-center">
-                  <div className="text-sm text-gray-500">Вуглеводи</div>
-                  <div className="font-medium">
-                    {recipe.nutrition?.carbs || "0"} г
-                  </div>
-                </div>
+            <div className="bg-gray-50 p-2 rounded-md text-center">
+              <div className="text-sm text-gray-500 md:text-base lg:text-lg">
+                Вуглеводи
+              </div>
+              <div className="font-medium md:text-lg lg:text-xl">
+                {recipe.nutrition?.carbs || "0"} г
               </div>
             </div>
           </div>
@@ -345,18 +370,6 @@ const RecipeDetail = ({ id, initialData = {} }) => {
             {recipe.ingredients &&
               recipe.ingredients.map((ingredient, index) => (
                 <li key={index} className="flex items-start">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-green-500 mr-2 flex-shrink-0 mt-0.5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
                   <span>{ingredient}</span>
                 </li>
               ))}
