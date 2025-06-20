2a0e29f154cdc1cbac1b11383fcc9ae041c8993b
KaterynaMarkevych <katya.markevich73@gmail.com>
2025-06-20 16:02:29 +0300

    fix 5

--- src/app/private-recipes/[id]/page.js
+++ src/app/private-recipes/[id]/page.js
@@ -5,46 +5,6 @@ import Wrapper from "@/components/shared/Wrapper/Wrapper";
 import UnpublishedRecipeDetail from "@/components/Recipes/UnpublishedRecipeDetail";
 import { redirect } from "next/navigation";
 
-function serializeValue(value) {
-  if (value == null) return value;
-
-  if (Array.isArray(value)) {
-    return value.map(serializeValue);
-  }
-
-  if (typeof value === "object") {
-    // ObjectId -> рядок
-    if (
-      value._bsontype === "ObjectID" &&
-      typeof value.toString === "function"
-    ) {
-      return value.toString();
-    }
-
-    // Buffer -> base64 рядок
-    if (Buffer.isBuffer(value)) {
-      return value.toString("base64");
-    }
-
-    // Uint8Array -> base64 рядок
-    if (value instanceof Uint8Array) {
-      return Buffer.from(value).toString("base64");
-    }
-
-    // Відкидаємо всі функції і поля, які є функціями (щоб React не падав)
-    const newObj = {};
-    for (const key in value) {
-      if (typeof value[key] !== "function") {
-        newObj[key] = serializeValue(value[key]);
-      }
-    }
-    return newObj;
-  }
-
-  // Примітивні типи залишаємо як є
-  return value;
-}
-
 export default async function UnpublishedRecipePage({ params }) {
   const session = await getServerSession(authOptions);
 
@@ -55,13 +15,12 @@ export default async function UnpublishedRecipePage({ params }) {
 
   try {
     const recipe = await getRecipeById(params.id, session.user.id);
-    const recipeObj = recipe.toObject();
-    const serializedRecipe = serializeValue(recipeObj);
+    const plainRecipe = JSON.parse(JSON.stringify(recipe));
 
     return (
       <main>
         <Wrapper>
-          <UnpublishedRecipeDetail recipe={serializedRecipe} />
+          <UnpublishedRecipeDetail recipe={plainRecipe} />
         </Wrapper>
       </main>
     );
