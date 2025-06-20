b57c836f9c07e4001e7fe4aeae927558a1372d8c
KaterynaMarkevych <katya.markevich73@gmail.com>
2025-06-20 11:10:52 +0300

    fix some variables

--- src/scripts/uploadToCloudinary.js
+++ src/scripts/uploadToCloudinary.js
@@ -4,8 +4,10 @@ export const uploadToCloudinary = async (file) => {
   formData.append("upload_preset", "user_avatar");
   formData.append("folder", "users/avatars");
 
+  const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+
   const response = await fetch(
-    "https://api.cloudinary.com/v1_1/dz1kvzidt/image/upload",
+    `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
     {
       method: "POST",
       body: formData,
